ad7aa4ce9e76a6c3c74faac108ec1f53b01510aa
Daniel Rocha <dgustavo@id.uff.br>
2021-07-05 01:24:52 -0300

    fix heroku endpoint

--- src/services/apiService.jsx
+++ src/services/apiService.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //Configuração global da API no AXIOS
 export const api = axios.create({
-    baseURL: "http://https://basic-node-api.herokuapp.com/", //heroku
+    baseURL: "https://basic-node-api.herokuapp.com/", //heroku
     //baseURL: "http://localhost:4242/", //dev
     headers: { "Content-Type": "application/json" },
   });
